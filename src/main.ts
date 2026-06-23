524f4f64da16a973470f162429f8f1f5f1a199f0
JesusGonzalez28 <tapiadavid2803@gmail.com>
2026-06-23 03:56:07 -0500

    Modify CORS and validation settings in bootstrap
    Updated CORS settings to allow all origins and modified validation pipe to not forbid non-whitelisted properties. Adjusted API description for clarity.

--- src/main.ts
+++ src/main.ts
@@ -11,34 +11,30 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const logger = new Logger('Bootstrap');
 
-  // Crear carpeta de uploads si no existe
   const uploadsDir = join(process.cwd(), 'uploads', 'vouchers');
   if (!existsSync(uploadsDir)) mkdirSync(uploadsDir, { recursive: true });
 
-  // Servir archivos estáticos (comprobantes)
   app.useStaticAssets(join(process.cwd(), 'uploads'), { prefix: '/uploads' });
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true,
+      forbidNonWhitelisted: false,
       transform: true,
       transformOptions: { enableImplicitConversion: true },
     }),
   );
 
   app.useGlobalFilters(new AllExceptionsFilter());
+
   app.enableCors({
-  origin: [
-    'https://front-end-gh51.onrender.com',
-    'http://localhost:5173',
-  ],
-  credentials: true,
-});
+    origin: '*',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('API de Rifas - Dinámicas Los Hermanos')
-    .setDescription('Backend para gestión de rifas, tickets y compras con flujo de pago por Nequi.')
+    .setDescription('Backend para gestión de rifas.')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
